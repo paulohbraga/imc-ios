cfcbe8215ac82bc0d1cbb7accd103ced007e4f47
Paulo Henrique Braga <paulobragapereira@gmail.com>
2021-01-25 18:52:44 -0300

    added outlets fo inputs, buttons and image view

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivImageResult: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateImc(_ sender: Any) {
+        debugPrint("teste")
+    }
+    
 }
 
